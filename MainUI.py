e5eb2be88b1d90a522f83c6efd1c9a78b8b52600
Jesse Sestito <jesse.sestito@valpo.edu>
2023-11-13 15:37:41 -0600

    Big updates

--- MainUI.py
+++ MainUI.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import time
+
+from datetime import datetime, timedelta
 
 from PyQt6 import QtWidgets, QtGui, QtCore
 from PyQt6.QtWidgets import QDialog, QApplication, QFileDialog, QWidget, QMainWindow, QButtonGroup, QMessageBox
@@ -9,8 +12,23 @@ from PyQt6.QtCore import QUrl
 from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PyQt6.QtMultimediaWidgets import QVideoWidget
 
+
+def fix_time(disp_string: str):
+    if len(disp_string) != 0:
+        while disp_string[0] == '0' or disp_string[0] == ":":
+            disp_string = disp_string[1:]
+            if len(disp_string) == 0:
+                break
+
+    if disp_string == '':
+        disp_string = '0'
+    
+    return disp_string
+
 class MainUI(QMainWindow):
-    ui_name = "MainUI.ui"
+    ui_name: str = "MainUI.ui"
+    video: str = "Countdown.mp4"
+    debug = True
 
     def __init__(self, data):
         self.data = data
@@ -22,7 +40,10 @@ class MainUI(QMainWindow):
         self.showMaximized()
         #self.showFullScreen()
 
-
+        self.enable_code(False)
+        self.codeStatus.setText("")
+        self.confirmCode.setText("")
+        
 
         self.media_player = QMediaPlayer()
         self.media_player.errorOccurred.connect(self._player_error)
@@ -36,13 +57,245 @@ class MainUI(QMainWindow):
 
         #self.mainLayout.addWidget(self.video_widget)
 
+        # Start Second
+        start_time = data["TIMING"]["StartTime"]
+        start_time = start_time.split(':')
+        self.start_second = (int(start_time[0]) * 60 + int(start_time[1])) * 60
 
-        self.pushButton.clicked.connect(self.start_video)
+
+        #Timer Stuff
+        # Second update timer
+        self.preupdateTimer = QtCore.QTimer(self)
+        self.preupdateTimer.setInterval(100)
+        self.preupdateTimer.timeout.connect(self.PreTimerCallback)
+
+        # Second update timer
+        self.postupdateTimer = QtCore.QTimer(self)
+        self.postupdateTimer.setInterval(100)
+        self.postupdateTimer.timeout.connect(self.PostTimerCallback)
+
+        
+
+        # Oneshot timers
+        self.textAppearTimer = QtCore.QTimer(self)
+        self.textAppearTimer.setSingleShot(True)
+        timeout = int(data["INTRO"]["TimeReappear"]) # Seconds
+        timeout = timeout * 1000
+        self.textAppearTimer.setInterval(timeout)
+        self.textAppearTimer.timeout.connect(self.AppearIntroTimer)
+        
+
+        self.textStartTimer = QtCore.QTimer(self)
+        self.textStartTimer.setSingleShot(True)
+        timeout = int(data["INTRO"]["TimeAfterVideo"]) # Seconds
+        timeout += int(data["INTRO"]["VideoLength"])
+        timeout = timeout * 1000
+        self.textStartTimer.setInterval(timeout)
+        self.textStartTimer.timeout.connect(self.StartIntroTimer)
+
+        self.startTTimer = QtCore.QTimer(self)
+        self.startTTimer.setSingleShot(True)
+        timeout = int(self.data["INTRO"]["TimeStartTimer"]) # Seconds
+        timeout = timeout * 1000
+        self.startTTimer.setInterval(timeout)
+        self.startTTimer.timeout.connect(self.Start)
+
+
+        timeout = int(data["INTRO"]["TimeAfterVideo"]) # Seconds
+        timeout += int(data["INTRO"]["VideoLength"])
+        timeout += int(data["INTRO"]["TimeStartTimer"])
+        self.startTime = timeout
+
+
+        self.confirmCode.clicked.connect(self.CheckCode)
+
+        code_data = data["CODE"]
+        self.code = code_data["Code"].upper()
+        self.lockout_time = int(code_data["LockoutTime"])
+        self.lockout_attempts = int(code_data["LockoutAttempts"])
+        self.attempts = 0
+
+        self.lockoutTimer = QtCore.QTimer(self)
+        self.lockoutTimer.setSingleShot(True)
+        timeout = int(code_data["LockoutTime"])
+        timeout = timeout * 1000
+        self.lockoutTimer.setInterval(timeout)
+        self.lockoutTimer.timeout.connect(self.Unlock)
+
+
+        self.statusResetTimer = QtCore.QTimer(self)
+        self.statusResetTimer.setSingleShot(True)
+        self.statusResetTimer.setInterval(5 * 1000)
+        self.statusResetTimer.timeout.connect(self.SetDefaultStatus)
+
+        
+        self.SetTimerState()
+        
+
+        
+
+    def SetTimerState(self):
+        now = datetime.now()
+        t = now.time()
+        seconds = (t.hour * 60 + t.minute) * 60 + t.second
+        if seconds < self.start_second:
+            self.preupdateTimer.start()
+        else:
+            self.Start()
+
+    def PostTimerCallback(self):
+        now = datetime.now()
+        t = now.time()
+        seconds = (t.hour * 60 + t.minute) * 60 + t.second
+        
+        # After we start
+        seconds_since_start = seconds - self.start_second
+        seconds_left = int(self.data["TIMING"]["Duration"]) * 60 - seconds_since_start
+
+        if seconds_left > 0:
+            # We are in competition
+            disp_string = str(timedelta(seconds = seconds_left))
+
+        else:
+            #ran out of time
+            disp_string = 'Time up!'
+            self.postupdateTimer.stop()
+            self.End()
+        
+        if len(disp_string) != 0:
+            while disp_string[0] == '0' or disp_string[0] == ":":
+                disp_string = disp_string[1:]
+                if len(disp_string) == 0:
+                    break
+
+        if disp_string == '':
+            disp_string = '0'
+
+        self.Clock.setText(disp_string)
+    
+    def PreTimerCallback(self):
+        now = datetime.now()
+        t = now.time()
+        seconds = (t.hour * 60 + t.minute) * 60 + t.second
         
 
 
-    def start_video(self):
-        video: str = "Countdown.mp4"
+        if seconds < self.start_second:
+            # Before we are supposed to start
+            seconds_until_start = self.start_second - seconds
+            disp_string = str(timedelta(seconds = seconds_until_start))
+
+            # TODO: What do I do if I crash mid video?
+            if seconds_until_start < self.startTime:
+                self.StartSequence()
+                return
+
+        else:
+            self.preupdateTimer.stop()
+            self.Start()
+
+
+        
+        if len(disp_string) != 0:
+            while disp_string[0] == '0' or disp_string[0] == ":":
+                disp_string = disp_string[1:]
+                if len(disp_string) == 0:
+                    break
+
+        if disp_string == '':
+            disp_string = '0'
+
+        self.Clock.setText(disp_string)
+
+
+
+    def StartSequence(self):
+        if self.debug: print('Initiate Start Sequence!')
+        self.start_video(self.video)
+        self.StopIntroTimer()
+        self.textAppearTimer.start()
+        self.textStartTimer.start()
+
+
+    def StopIntroTimer(self):
+        if self.debug: print('Stop Intro Timer')
+        self.preupdateTimer.stop()
+        self.Clock.setText("")
+
+    def AppearIntroTimer(self):
+        if self.debug: print('Appear Timer')
+        time = int(self.data["TIMING"]["Duration"]) * 60
+        disp_string = str(timedelta(seconds = time))
+        disp_string = fix_time(disp_string)   
+        self.Clock.setText(disp_string)
+
+    def StartIntroTimer(self):
+        if self.debug: print('Initiating Timer')
+        # Play audio
+
+
+        seconds_to_wait = int(self.data["INTRO"]["TimeStartTimer"])
+        self.startTTimer.start()
+
+    def Start(self):
+        if self.debug: print('Starting Game')
+        #unlock buttons
+        self.enable_code()
+        self.confirmCode.setText("Confirm Code")
+        self.SetDefaultStatus()
+
+        self.postupdateTimer.start()
+
+    def End(self):
+        # Lock code enter thing
+        self.lockoutTimer.stop()
+
+
+    def CheckCode(self):
+        self.update_status("Checking Code...")
+        guess = self.codeEntry.text().upper()
+        if guess == self.code:
+            self.Win()
+        else:
+            self.attempts += 1
+            # status wrong code
+            self.WrongCodeStatus(guess)
+            if self.attempts >= self.lockout_attempts:
+                self.Lockout()
+                self.attempts = 0
+
+    def WrongCodeStatus(self, guess):
+        self.update_status("Wrong Code!")
+
+    def Lockout(self):
+        self.statusResetTimer.stop()
+        self.enable_code(False)
+        self.update_status("Locked Out!")
+        self.lockoutTimer.start()
+
+    def Unlock(self):
+        self.enable_code(True)
+        self.SetDefaultStatus()
+
+    def Win(self):
+        self.enable_code(False)
+        self.postupdateTimer.stop()
+        self.Clock.setText('You Win!')
+        self.codeStatus.setText('You Win!')
+
+    def SetDefaultStatus(self):
+        self.codeStatus.setText("Awaiting Code...")
+
+    def update_status(self, text):
+        self.codeStatus.setText(text)
+        self.statusResetTimer.stop()
+        self.statusResetTimer.start()
+
+    def enable_code(self, state = True):
+        self.codeEntry.setEnabled(state)
+        self.confirmCode.setEnabled(state)
+
+    def start_video(self, video):
         default_path = os.path.dirname(os.path.realpath(__file__))
         file_path = os.path.join(default_path, video)
 
